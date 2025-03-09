f63ebaf05453222a36fc757dc2108d92962b20c5
Raufull Islam Rauf <rauf.shuvo3272@gmail.com>
2025-03-09 18:56:52 +0600

    Updated Global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { HeroSection } from "@/components/sections/HeroSection";
+import { AboutSection } from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
